5de0b3010421a7ed7eae23d49ac84a7f8c328001
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2025-04-01 18:05:32 +0100

    working with vec

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,63 @@
-use std::io;
+    use std::io;
 
-struct Gasto {
-    descripcion: String,
-    monto: f64, // f64 es para numero decimales.
-}
+    struct Gasto {
+        descripcion: String,
+        monto: f64, // f64 es para numero decimales.
+    }
 
-fn main() {
-    // Crear variables para almacenar los datos ingresados por el usuario.
-    let mut descripcion = String::new();
-    let mut monto = String::new();
+    fn main() {
 
-    // Pedir al usuario que ingrese la descripción del gasto.
-    println!("¿Cuál es la descripción del gasto?");
-    io::stdin()
-        .read_line(&mut descripcion)
-        .expect("Error al leer la entrada");
+        let mut gastos: Vec<Gasto> = Vec::new();
 
-    // Pedir al usuario que ingrese el monto del gasto.
-    println!("¿Cuál es el monto del gasto?");
-    io::stdin()
-        .read_line(&mut monto)
-        .expect("Error al leer la entrada");
 
-    // Convertir el monto ingresado (que es texto) a un número decimal.
-    let monto: f64 = monto.trim().parse().expect("Por favor ingresa un número válido");
+    loop{
 
-    // Crear una instancia del struct Gasto con los datos ingresados.
-    let gasto1 = Gasto {
-        descripcion: descripcion.trim().to_string(),
-        monto,
-    };
+        // Crear variables para almacenar los datos ingresados por el usuario.
+        let mut descripcion = String::new();
+        let mut monto = String::new();
 
-    // Mostrar los datos del gasto creado.
-    println!(
-        "Has registrado un gasto de '{}' por ${:.2}",
-        gasto1.descripcion, gasto1.monto
-    );
-}
+    
+
+
+        // Pedir al usuario que ingrese la descripción del gasto.
+        println!("¿Cuál es la descripción del gasto? o escriba salir para terminar");
+        io::stdin()
+            .read_line(&mut descripcion)
+            .expect("Error al leer la entrada");
+
+    if descripcion.trim().eq_ignore_ascii_case("salir"){
+        break;
+    }
+
+
+        // Pedir al usuario que ingrese el monto del gasto.
+        println!("¿Cuál es el monto del gasto?");
+        io::stdin()
+            .read_line(&mut monto)
+            .expect("Error al leer la entrada");
+
+        // Convertir el monto ingresado (que es texto) a un número decimal.
+        let monto: f64 = match monto.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Porfavor ingrese un numero valido");
+            continue;
+        }
+
+        };
+
+        gastos.push(Gasto {
+            descripcion: descripcion.trim().to_string(),
+            monto,
+        });
+        println!("Gasto agregado correctamente!\n");
+    }
+
+    println!("\nResumen de gastos:");
+    for gasto in &gastos{
+        println!("- {}: ${:.2}", gasto.descripcion, gasto.monto);
+    }
+    let total: f64 = gastos.iter().map(|g| g.monto).sum();
+    println!("Total gastado: ${:.2}", total);
+
+    }
