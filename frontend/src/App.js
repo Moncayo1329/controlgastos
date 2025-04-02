d543296f4059830a70feee6ff249216d7fe97142
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2025-04-02 17:48:33 +0100

    install frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>La yapita</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
