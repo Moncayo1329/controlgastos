b9ed6cbbf956c34bfbedc357881df6b067fc4690
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2025-04-02 10:55:13 +0100

    done enum categorias

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
     use std::io;
+    #[derive(Debug)]
+    enum Categoria {
+        Alimentos,
+        Transporte,
+        Entretenimiento,
+        Comida,
+        Otros, 
+    }
 
     struct Gasto {
         descripcion: String,
         monto: f64, // f64 es para numero decimales.
+        categoria:Categoria,
     }
 
     fn main() {
@@ -15,6 +24,7 @@
         // Crear variables para almacenar los datos ingresados por el usuario.
         let mut descripcion = String::new();
         let mut monto = String::new();
+        let mut  categoria = String::new();
 
     
 
@@ -30,11 +40,36 @@
     }
 
 
-        // Pedir al usuario que ingrese el monto del gasto.
+        // Pedir al usuario que ingrese el gasto 
         println!("¿Cuál es el monto del gasto?");
         io::stdin()
             .read_line(&mut monto)
             .expect("Error al leer la entrada");
+
+// Pedir al usuario que ingrese el gasto 
+
+            println!("¿Cuál es la categoria del gasto");
+        io::stdin()
+            .read_line(&mut categoria)
+            .expect("Error al leer la categoria");
+
+
+      // Categoria 
+      let categoria = match categoria.trim().to_lowercase().as_str(){
+        "comida" => Categoria::Comida,
+        "transporte" => Categoria::Transporte,
+        "entretenimiento" => Categoria::Entretenimiento,
+      "alimentos" => Categoria::Alimentos,
+        "otros" => Categoria::Otros,
+        _ => {
+            println!("Categoría inválida. Intente nuevamente.");
+            continue;
+        }
+    };
+
+ 
+
+
 
         // Convertir el monto ingresado (que es texto) a un número decimal.
         let monto: f64 = match monto.trim().parse() {
@@ -49,13 +84,14 @@
         gastos.push(Gasto {
             descripcion: descripcion.trim().to_string(),
             monto,
+            categoria,
         });
         println!("Gasto agregado correctamente!\n");
     }
 
     println!("\nResumen de gastos:");
     for gasto in &gastos{
-        println!("- {}: ${:.2}", gasto.descripcion, gasto.monto);
+        println!("- {}: ${:.2} ({:?})", gasto.descripcion, gasto.monto, gasto.categoria);
     }
     let total: f64 = gastos.iter().map(|g| g.monto).sum();
     println!("Total gastado: ${:.2}", total);
