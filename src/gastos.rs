ffe76ac7e0d6045f81442c7f9b8b7459d1fa3c75
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2025-04-11 15:19:55 +0100

    learn about modules,derive and pub

---
+++ src/gastos.rs
@@ -0,0 +1,43 @@
+// gastos.rs
+
+use std::fmt;
+
+
+#[derive(Debug,Clone,Copy,PartialEq)]
+
+pub enum Categoria {
+Alimentos,
+Transporte,
+Entretenimiento,
+Comida,
+Otros,
+
+}
+
+
+
+#[derive(Debug)]
+
+
+pub struct Gasto {
+    pub descripcion:String,
+    pub monto: f64,
+    pub categoria: Categoria.
+
+}
+
+
+impl fmt::Display for Gasto {
+
+fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+    write!(f, "{} -${:.2} ({:?})", self.descripcion, self.monto, self.categoria)
+}
+
+}
+
+
+// filtrar x gastos 
+
+pub  fn filtrar_por_categoria(gastos: &Vec<Gasto>, categoria:Categoria) -> Vec<&Gasto> {
+    gastos.iter().filter(|g| g.categoria == categoria).collect()
+}
