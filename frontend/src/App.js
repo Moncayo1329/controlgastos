a178f80d1e326b1483f210738f9233012129d712
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2025-04-21 10:26:41 +0100

    start working frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
 import './App.css'; 
 import Gastos from './yapita';
+import Principal from './Inicio';
 
 function App() {
   return (
-    <div className="App">
-     
-       <h1>La yapita</h1>
-       <h3>Si no te mueres manana es mejor controlar tus gastos.</h3>
-       <h1>Controla tus gastos :</h1>
-       <Gastos />
+    <div>
+      <Principal />
+      <Gastos />
     </div>
+     
   );
 }
 
---
+++ frontend/src/Inicio.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function Principal(){
+
+    return(
+    <div className="App d-flex flex-column align-items-center p-4 gap-4">
+     <h1>La yapita</h1>
+     <h3>Si no te mueres manana es mejor controlar tus gastos.</h3>
+     <h1>Controla tus gastos :</h1>
+
+
+    </div>
+
+)
+}
+
+
+
+export default Principal;
