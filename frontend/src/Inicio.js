6592b44a771fb9f467f1fba57ca44565a2f0903a
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2025-04-22 10:49:32 +0100

    continue working styles

--- frontend/src/Inicio.js
+++ frontend/src/Inicio.js
@@ -6,7 +6,7 @@ function Principal(){
     return(
     <div className="App d-flex flex-column align-items-center p-4 gap-4">
      <h1>La yapita</h1>
-     <h3>Si no te mueres manana es mejor controlar tus gastos.</h3>
+     <h3>Si no te mueres mañana es mejor controlar tus gastos.</h3>
      <h1>Controla tus gastos :</h1>
 
 
